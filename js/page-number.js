6924fce13344ad31768a40e3b37ed6523360171b
AuroreSoleilevant <darenzhou666@gmail.com>
2025-10-27 11:07:08 +0100

    Site updated: 2025-10-27 11:07:07

--- js/page-number.js
+++ js/page-number.js
@@ -3,17 +3,17 @@
 (function () {
   const CONFIG = {
     maxPages: {
-      histoire: 3, // 故事区最大页数，目前故事15
+      histoire: 3, // 故事区最大页数，目前故事16
       article: 1, // 文章区最大页数，目前文章1
       // 分类页面配置 - 每个分类单独设置最大页数
       "tag/musique": 1, //1
       "tag/long": 1, //2
-      "tag/fini": 3, //15
-      "tag/MLP": 2, //11
+      "tag/fini": 3, //16
+      "tag/MLP": 2, //12
       "tag/apaisant": 1, //4
       "tag/amour": 2, //7
       "tag/moyenne": 1, //5
-      "tag/court": 2, //8
+      "tag/court": 2, //9
       "tag/comedie": 1, //3
       "tag/suspense": 1, //2
       "tag/epouvante": 1, //1
