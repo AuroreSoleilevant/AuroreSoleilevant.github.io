952fb1d2524f60557d98bb7401909f543fe51870
AuroreSoleilevant <darenzhou666@gmail.com>
2025-11-01 18:10:22 +0100

    Site updated: 2025-11-01 18:10:21

---
+++ js/special/giscus-peur.js
@@ -0,0 +1,27 @@
+(function () {
+  const script = document.createElement("script");
+  script.src = "https://giscus.app/client.js";
+  script.async = true;
+  script.crossOrigin = "anonymous";
+
+  // 设置 Giscus 参数
+  script.setAttribute(
+    "data-repo",
+    "AuroreSoleilevant/AuroreSoleilevant.github.io"
+  );
+  script.setAttribute("data-repo-id", "R_kgDOP4baWw");
+  script.setAttribute("data-category", "Announcements");
+  script.setAttribute("data-category-id", "DIC_kwDOP4baW84Cwye2");
+  script.setAttribute("data-mapping", "pathname");
+  script.setAttribute("data-strict", "1");
+  script.setAttribute("data-reactions-enabled", "1");
+  script.setAttribute("data-emit-metadata", "0");
+  script.setAttribute("data-input-position", "top");
+  script.setAttribute("data-theme", "gruvbox_dark");
+  script.setAttribute("data-lang", "zh-CN");
+  script.setAttribute("data-loading", "lazy");
+
+  // 插入到评论容器中
+  const target = document.getElementById("giscus-container") || document.body;
+  target.appendChild(script);
+})();
